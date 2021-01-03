7d4bfe03fc961d7e0a670ab32ee571cac5238c26
Sho Kurosu <xsho0212@outlook.com>
2021-01-04 05:10:22 +0900

    nemu

---
+++ raycast.js
@@ -0,0 +1,60 @@
+const TILE_SIZE = 32;
+const MAP_NUM_ROWS = 11;
+const MAP_NUM_COLS = 15;
+
+const WINDOW_WIDTH = MAP_NUM_COLS * TILE_SIZE;
+const WINDOW_HEIGHT = MAP_NUM_ROWS * TILE_SIZE;
+
+class Map
+{
+	constructor()
+	{
+		this.grid = [
+			[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+			[1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+			[1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+			[1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+			[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+		];
+	}
+
+	render()
+	{
+		for (let i = 0; i < MAP_NUM_ROWS; i++)
+		{
+			for (let j = 0; j < MAP_NUM_COLS; j++)
+			{
+				let tileX = j * TILE_SIZE;
+				let tileY = i * TILE_SIZE;
+				var tileColor = (this.grid[i][j] == 1 ? "#222" : "#fff");
+				stroke("#222");
+				fill(tileColor);
+				rect(tileX, tileY, TILE_SIZE, TILE_SIZE);
+			}
+		}
+	}
+}
+
+let grid = new Map();
+
+function setup()
+{
+	createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
+}
+
+function update()
+{
+	// update all game objects
+}
+
+function draw()
+{
+	update();
+	grid.render();
+}
